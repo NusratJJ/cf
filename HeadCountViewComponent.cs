02b0584f99d3981dc0fba5cccd7029cb35657aac
Mst.Nusrat Jahan Jisa <njjisa499@gmail.com>
2025-02-19 10:37:24 +0600

    Add files via upload

---
+++ HeadCountViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using CoreWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreWeb.ViewComponents
+{
+    
+    public class HeadCountViewComponent:ViewComponent
+    {
+        private readonly WeB1dbContext _db;
+
+        public HeadCountViewComponent(WeB1dbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IViewComponentResult>InvokeAsync(int courseId)
+        {
+            var courseCounts = await _db.Students
+                .Include(s => s.Course).GroupBy(s => new { s.Course.CourseId, s.Course.CourseName }).
+                Select(g => new CourseHeadCount
+                {
+                    CourseId = g.Key.CourseId,
+                    CourseName = g.Key.CourseName,
+                    Count = g.Count()
+                }).ToListAsync();
+            return View(courseCounts);
+        }
+    }
+}
