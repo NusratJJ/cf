1c52953e12709061734f64def417c998947eb0ca
Mst.Nusrat Jahan Jisa <njjisa499@gmail.com>
2025-02-19 10:40:55 +0600

    Update StudentsController.cs

--- StudentsController.cs
+++ StudentsController.cs
@@ -65,3 +65,24 @@ namespace CoreWeb.Controllers
         }
     }
 }
+ public class HeadCountViewComponent:ViewComponent
+ {
+     private readonly WeB1dbContext _db;
+
+     public HeadCountViewComponent(WeB1dbContext db)
+     {
+         _db = db;
+     }
+     public async Task<IViewComponentResult>InvokeAsync(int courseId)
+     {
+         var courseCounts = await _db.Students
+             .Include(s => s.Course).GroupBy(s => new { s.Course.CourseId, s.Course.CourseName }).
+             Select(g => new CourseHeadCount
+             {
+                 CourseId = g.Key.CourseId,
+                 CourseName = g.Key.CourseName,
+                 Count = g.Count()
+             }).ToListAsync();
+         return View(courseCounts);
+     }
+ }
